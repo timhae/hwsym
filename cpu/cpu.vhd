9b62a3205d78f27339aa7e78fef27d05c6b8743b
hsm <hsm@tu-dresden.de>
2019-11-26 16:23:15 +0100

    start cpu

---
+++ cpu/cpu.vhd
@@ -0,0 +1,13 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity cpu is
+    Port(clk, rst, slct, start : in std_logic);
+end cpu;
+
+architecture Behavioral of cpu is
+
+begin
+
+
+end Behavioral;
