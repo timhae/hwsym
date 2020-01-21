9dce531748e243a16b1861d539d9b8b7d3175032
Tim Haering <tim.haering@gmail.com>
2020-01-21 15:03:07 +0100

    more lab9 stuff

---
+++ lab9_trafficlight/main.cpp
@@ -0,0 +1,20 @@
+/*
+ * main.cpp
+ *
+ *  Created on: Jan 14, 2018
+ *      Author: Julian Haase
+ */
+
+#include <systemc.h>
+#include "tb_timer_top.h"
+
+tb_timer_top 		*tb_timer_top0 		= NULL;
+
+int sc_main (int argc, char* argv[]) {
+
+	% Extend here depent on the given Task!
+
+	cout << "Finished." << endl;
+	return 0;
+}
+
